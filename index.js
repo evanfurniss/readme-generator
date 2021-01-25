b294f1868fa983b8e42746ad8930f4b3ed110cfa
Evan Furniss <evanfurniss@gmail.com>
2021-01-25 16:53:56 -0600

    Added gitignore to repo

--- index.js
+++ index.js
@@ -6,42 +6,43 @@ const fs = require("fs");
 const questions = [
     {
         type: "input",
-        question: "What is the title of your project?",
+        message: "What is the title of your project?",
         name: "title"
     },
     {
         type: "input",
-        question: "How would you describe this project?",
+        message: "How would you describe this project?",
         name: "description"
     },
     {
         type: "input",
-        question: "What are the steps to install this program?",
+        message: "What are the steps to install this program?",
         name: "installation"
     },
     {
         type: "input",
-        question: "How will end-users use your product?",
+        message: "How will end-users use your product?",
         name: "usage"
     },
     {
         type: "list",
-        question: "What license is would you like to use? (use arrow keys to navigate)",
-        name: "license"
+        message: "What license would you like to use? (use arrow keys to navigate)",
+        name: "license",
+        choices: "[]"
     },
     {
         type: "input",
-        question: "How can others contribute to this project?",
+        message: "How can others contribute to this project?",
         name: "contributors"
     },
     {
         type: "input",
-        question: "What tests were written for this README?",
+        message: "What tests were written for this README?",
         name: "test"
     },
     {
         type: "input",
-        question: "Where can someone go to ask questions about this project?",
+        message: "Where can people go to ask questions about this project?",
         name: "questions"
     }
 ];
