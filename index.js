f88a7ac722ad17c15b68856121baf606f641f3a9
Evan Furniss <evanfurniss@gmail.com>
2021-01-25 16:42:30 -0600

    Created questions to be asked

--- index.js
+++ index.js
@@ -5,7 +5,44 @@ const fs = require("fs");
 // TODO: Create an array of questions for user input
 const questions = [
     {
-
+        type: "input",
+        question: "What is the title of your project?",
+        name: "title"
+    },
+    {
+        type: "input",
+        question: "How would you describe this project?",
+        name: "description"
+    },
+    {
+        type: "input",
+        question: "What are the steps to install this program?",
+        name: "installation"
+    },
+    {
+        type: "input",
+        question: "How will end-users use your product?",
+        name: "usage"
+    },
+    {
+        type: "list",
+        question: "What license is would you like to use? (use arrow keys to navigate)",
+        name: "license"
+    },
+    {
+        type: "input",
+        question: "How can others contribute to this project?",
+        name: "contributors"
+    },
+    {
+        type: "input",
+        question: "What tests were written for this README?",
+        name: "test"
+    },
+    {
+        type: "input",
+        question: "Where can someone go to ask questions about this project?",
+        name: "questions"
     }
 ];
 
