dded836d412e2724b25fdfc38c43e479bed12f69
Evan Furniss <evanfurniss@gmail.com>
2021-01-25 19:40:31 -0600

    Added default and validate modules to verify input is being accepted and a default option

--- index.js
+++ index.js
@@ -1,64 +1,87 @@
 // TODO: Include packages needed for this application
 const inquire = require("inquirer");
 const fs = require("fs");
+const markdown = require("./util/generateMarkdown")
 
 // TODO: Create an array of questions for user input
 const questions = [
     {
         type: "input",
         message: "What is the title of your project?",
-        name: "title"
+        name: "title",
+        default: "Project Title",
+        validate: (answer) => (answer.length < 1) ? console.log("Input required") : true
     },
     {
         type: "input",
         message: "How would you describe this project?",
-        name: "description"
+        name: "description",
+        default: "Description",
+        validate: (answer) => (answer.length < 1) ? console.log("Input required") : true
     },
     {
         type: "input",
         message: "What are the steps to install this program?",
-        name: "installation"
+        name: "installation",
+        default: "Installation Instructions",
+        validate: (answer) => (answer.length < 1) ? console.log("Input required") : true
     },
     {
         type: "input",
         message: "How will end-users use your product?",
-        name: "usage"
+        name: "usage",
+        default: "Usage",
+        validate: (answer) => (answer.length < 1) ? console.log("Input required") : true
     },
     {
         type: "list",
         message: "What license would you like to use? (use arrow keys to navigate)",
         name: "license",
-        choices: ['GNU','MIT','Apache', 'Default']
+        choices: ['GNU','MIT','Apache', 'Default'],
+        default: "License",
+        validate: (answer) => (answer.length < 1) ? console.log("Input required") : true
     },
     {
         type: "input",
         message: "How can others contribute to this project?",
-        name: "contributors"
+        name: "contributors",
+        default: "Contributors",
+        validate: (answer) => (answer.length < 1) ? console.log("Input required") : true
     },
     {
         type: "input",
         message: "What tests were written for this README?",
-        name: "test"
+        name: "test",
+        default: "Testing",
+        validate: (answer) => (answer.length < 1) ? console.log("Input required") : true
     },
     {
         type: "input",
         message: "Where can people go to ask questions about this project?",
-        name: "questions"
+        name: "questions",
+        default: "Questions",
+        validate: (answer) => (answer.length < 1) ? console.log("Input required") : true
     },
     {
         type: "input",
         message: "Please input your GitHub username",
-        name: "git"
+        name: "git",
+        default: "GitHub",
+        validate: (answer) => (answer.length < 1) ? console.log("Input required") : true
     },
     {
         type: "input",
         message: "Please enter your email",
-        name: "email"
+        name: "email",
+        default: "Email",
+        validate: (answer) => (answer.length < 1) ? console.log("Input required") : true
     },
     {
         type: "input",
         message: "Please enter your LinkedIn username",
-        name: "linkedin"
+        name: "linkedin",
+        default: "LinkedIn",
+        validate: (answer) => (answer.length < 1) ? console.log("Input required") : true
     }
 ];
 
